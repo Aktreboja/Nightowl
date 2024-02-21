5fcd587ba92cfe99c73eb31775f5a36cdef56e93
Aldrich Reboja <aktreboja@gmail.com>
2024-02-21 10:13:14 -0800

    Refactoring

---
+++ types.d.ts
@@ -0,0 +1,17 @@
+
+
+export interface TokenResponse {
+    access_token: string;
+    refresh_token: string;
+    expires_in: number;
+  }
+  
+  export interface CurrentToken {
+    access_token: string | null;
+    refresh_token: string | null;
+    expires_in: string | null;
+    expires: string | null;
+  
+    save(response: TokenResponse): void;
+  }
+  
